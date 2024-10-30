84259a70cd957eb939f05f29c91cf6e116e77b50
Godgirl1 <135866332+Godgirl1@users.noreply.github.com>
2024-10-30 17:57:11 -0400

    Return Code
    Code didn't work so I'm putting it back to before :,)

--- objectEfield/Assets/arrowMaker.cs
+++ objectEfield/Assets/arrowMaker.cs
@@ -1,66 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit;
 
-public class ArrowMaker : MonoBehaviour
+public class arrowMaker : MonoBehaviour
 {
     public GameObject arrowPrefab;
     public Transform parentT;
-    private bool isGrabbed = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Initially do not create arrows
-    }
-
-    // Method called when the object is grabbed
-    public void OnGrab()
-    {
-        if (!isGrabbed)
+        for (int i = 0; i < 360; i += 15) //This is phi
         {
-            CreateArrows();
-            isGrabbed = true; // Prevent multiple calls
-        }
-    }
-
-    private void CreateArrows()
-    {
-        for (int i = 0; i < 360; i += 15) // This is phi
-        {
-            for (int j = 0; j < 180; j += 15) // This is theta
+            for (int j = 0; j < 180; j += 15) //This is theta
             {
-                var arrow = Instantiate(arrowPrefab, parentT);
-                Vector3 arrowDir = new Vector3(
-                    Mathf.Sin(j * Mathf.Deg2Rad) * Mathf.Cos(i * Mathf.Deg2Rad),
-                    Mathf.Sin(j * Mathf.Deg2Rad) * Mathf.Sin(i * Mathf.Deg2Rad),
-                    Mathf.Cos(j * Mathf.Deg2Rad)
-                );
-
+                var arrow = Instantiate<GameObject>(arrowPrefab, parentT);
+                Vector3 arrowDir = new Vector3(Mathf.Sin(j * Mathf.Deg2Rad) * Mathf.Cos(i * Mathf.Deg2Rad), Mathf.Sin(j * Mathf.Deg2Rad) * Mathf.Sin(i * Mathf.Deg2Rad), Mathf.Cos(j * Mathf.Deg2Rad));
                 arrow.transform.Translate(0.1f * arrowDir);
-                
-                var arrow2 = Instantiate(arrowPrefab, parentT);
+                var arrow2 = Instantiate<GameObject>(arrowPrefab, parentT);
                 arrow2.transform.Translate(0.4f * arrowDir);
-                arrow2.transform.Rotate(Mathf.Sin(j * Mathf.Deg2Rad) * Mathf.Cos(i * Mathf.Deg2Rad), 
-                                        Mathf.Sin(j * Mathf.Deg2Rad) * Mathf.Sin(i * Mathf.Deg2Rad), 
-                                        Mathf.Cos(j * Mathf.Deg2Rad));
-                arrow2.transform.localEulerAngles = new Vector3(
-                    Mathf.Sin(j * Mathf.Deg2Rad) * Mathf.Cos(i * Mathf.Deg2Rad),
-                    Mathf.Sin(j * Mathf.Deg2Rad) * Mathf.Sin(i * Mathf.Deg2Rad),
-                    Mathf.Cos(j * Mathf.Deg2Rad)
-                );
-
+                arrow2.transform.Rotate(Mathf.Sin(j * Mathf.Deg2Rad) * Mathf.Cos(i * Mathf.Deg2Rad), Mathf.Sin(j * Mathf.Deg2Rad) * Mathf.Sin(i * Mathf.Deg2Rad), Mathf.Cos(j * Mathf.Deg2Rad));
+                arrow2.transform.localEulerAngles = new Vector3(Mathf.Sin(j * Mathf.Deg2Rad) * Mathf.Cos(i * Mathf.Deg2Rad), Mathf.Sin(j * Mathf.Deg2Rad) * Mathf.Sin(i * Mathf.Deg2Rad), Mathf.Cos(j * Mathf.Deg2Rad));
                 arrow.transform.LookAt(parentT);
                 arrow2.transform.LookAt(parentT);
             }
+
         }
+
     }
 
-    // Called when the object is released
-    public void OnRelease()
+    // Update is called once per frame
+    void Update()
     {
-        // Optional: handle behavior when released, if needed
-        isGrabbed = false;
+
     }
 }
