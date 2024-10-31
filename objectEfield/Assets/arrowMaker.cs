4f72739c9d9512a5d22e6b11944369c24ca2f58b
Godgirl1 <135866332+Godgirl1@users.noreply.github.com>
2024-10-31 14:51:49 -0400

    Radiating Arrow
    Turns out by assigning calling ParentT, you are assigning the sphere as the parent and using its coordinates, so no need for extra coordinate getting!
    
    Anyway, I modified the code to crate a loop for multiple arrow. IT. IS. HORRIFYING right now, but I'll try spacing out the arrows next week.

--- objectEfield/Assets/arrowMaker.cs
+++ objectEfield/Assets/arrowMaker.cs
@@ -10,23 +10,29 @@ public class arrowMaker : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 360; i += 15) //This is phi
+        int arrowCount = 10; // Number of additional arrows
+        float baseDistance = 0.4f; // Starting distance
+        float distanceIncrement = 1.0f; // Distance increment for each set of arrows
+
+        for (int i = 0; i < 360; i += 15) // This is phi
         {
-            for (int j = 0; j < 180; j += 15) //This is theta
+            for (int j = 0; j < 180; j += 15) // This is theta
             {
-                var arrow = Instantiate<GameObject>(arrowPrefab, parentT);
-                Vector3 arrowDir = new Vector3(Mathf.Sin(j * Mathf.Deg2Rad) * Mathf.Cos(i * Mathf.Deg2Rad), Mathf.Sin(j * Mathf.Deg2Rad) * Mathf.Sin(i * Mathf.Deg2Rad), Mathf.Cos(j * Mathf.Deg2Rad));
-                arrow.transform.Translate(0.1f * arrowDir);
-                var arrow2 = Instantiate<GameObject>(arrowPrefab, parentT);
-                arrow2.transform.Translate(0.4f * arrowDir);
-                arrow2.transform.Rotate(Mathf.Sin(j * Mathf.Deg2Rad) * Mathf.Cos(i * Mathf.Deg2Rad), Mathf.Sin(j * Mathf.Deg2Rad) * Mathf.Sin(i * Mathf.Deg2Rad), Mathf.Cos(j * Mathf.Deg2Rad));
-                arrow2.transform.localEulerAngles = new Vector3(Mathf.Sin(j * Mathf.Deg2Rad) * Mathf.Cos(i * Mathf.Deg2Rad), Mathf.Sin(j * Mathf.Deg2Rad) * Mathf.Sin(i * Mathf.Deg2Rad), Mathf.Cos(j * Mathf.Deg2Rad));
-                arrow.transform.LookAt(parentT);
-                arrow2.transform.LookAt(parentT);
+                // Create arrows at increasing distances
+                for (int k = 0; k < arrowCount; k++)
+                {
+                    float currentDistance = baseDistance + k * distanceIncrement;
+                    var arrow = Instantiate<GameObject>(arrowPrefab, parentT);
+                    Vector3 arrowDir = new Vector3(Mathf.Sin(j * Mathf.Deg2Rad) * Mathf.Cos(i * Mathf.Deg2Rad),
+                                                    Mathf.Sin(j * Mathf.Deg2Rad) * Mathf.Sin(i * Mathf.Deg2Rad),
+                                                    Mathf.Cos(j * Mathf.Deg2Rad));
+                    arrow.transform.Translate(currentDistance * arrowDir);
+
+                    // Rotate the arrow to face the center
+                    arrow.transform.LookAt(parentT);
+                }
             }
-
         }
-
     }
 
     // Update is called once per frame
